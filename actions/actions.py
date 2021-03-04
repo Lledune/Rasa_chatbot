afd6aec813ba235eaf751f7f823f6b07b5d9c51f
= <lucien.ledune.ds@gmail.com>
2021-03-04 16:09:29 +0100

    delete test scripts

--- actions/actions.py
+++ actions/actions.py
@@ -24,8 +24,8 @@ class ActionCoffeeList(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text = "Here are the coffee types that we sell : ")
-        dispatcher.utter_message(text="- Americano \n - Cappucino \n - Coffee")
+        dispatcher.utter_message(text = "Here are the coffee types that we sell :")
+        dispatcher.utter_message(text=" - Americano \n - Cappucino \n - Espresso")
 
         return []
 
@@ -42,7 +42,7 @@ class ActionSendCoffeeEmail(Action):
         coffee = tracker.get_slot("coffee")
 
         #SEND EMAIL 
-        sg = sendgrid.SendGridAPIClient(api_key='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')
+        sg = sendgrid.SendGridAPIClient(api_key='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') #Free api key, purposely not hidden.
         from_email = Email("lucien.ledune.ds@gmail.com")  # Change to your verified sender
         to_email = To(email)  # Change to your recipient
         subject = "Your coffee order"
